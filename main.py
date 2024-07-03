c50bacb8f3610e935095071d69f040dcfb3ab682
alexanderal-shekhly <alexander.marabu@gmail.com>
2024-07-03 09:28:35 +0200

    Ver 1.2

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from tkinter import messagebox
 import secrets
 import string
 import pyperclip
+import json
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
@@ -16,23 +17,74 @@ def generate_password():
     password_entry.insert(0, password)
 
 
+# ---------------------------- FIND PASSWORD ------------------------------- #
+
+
+def find_password():
+    website = website_entry.get()
+
+    try:
+        with open("passwords.json", "r") as password_file:
+            # Reading old data
+            data = json.load(password_file)
+    except FileNotFoundError:
+        messagebox.showerror(title="Error", message="No Data File Found")
+    except json.JSONDecodeError:
+        messagebox.showerror(title="Error", message="No password is yet saved")
+    else:
+        if website in data:
+            messagebox.showinfo(title=f"{website}", message=f"\nWebsite: {website}" 
+                                                            f"\nEmail: {data[website]["email"]} " 
+                                                            f"\nPassword: {data[website]["password"]}")
+        else:
+            messagebox.showerror(title="Error", message="No details for the website exists")
+
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 
 def save_password():
-
     website = website_entry.get()
     email = email_username_entry.get()
     password = password_entry.get()
-
+    new_data = {
+        website: {
+            "email": email,
+            "password": password
+        }
+    }
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="Oops", message="Please don't leave any fields empty!")
     else:
-        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} "
+        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nWebsite: {website}"
+                                                              f"\nEmail: {email} "
                                                               f"\nPassword: {password} \nIs it ok to save?")
         if is_ok:
-            with open("passwords.txt", "a") as password_file:
-                password_file.write(f"{website} | {email} | {password}\n")
+            try:
+                with open("passwords.json", "r") as password_file:
+                    # Reading old data
+                    data = json.load(password_file)
+                    # Checking if the entry is already in database
+                    if website in data:
+                        overwrite_data_ok = messagebox.askokcancel(title="Website already exists in database",
+                                                                   message=f"The website: "
+                                                                           f"{website} already exists in database"
+                                                                           f"\nDo you want to update password?")
+                        if overwrite_data_ok is not True:
+                            messagebox.showinfo(title=f"{website} not updated",
+                                                message=f"Password for {website} NOT updated")
+                            return
+            except (FileNotFoundError, json.JSONDecodeError):
+                # Handling errors if passwords.json doesn't exist or is empty
+                with open("passwords.json", "w") as password_file:
+                    json.dump(new_data, password_file, indent=4)
+            else:
+                data.update(new_data)
+                with open("passwords.json", "w") as password_file:
+                    json.dump(data, password_file, indent=4)
+
+            finally:
+                # Clearing entries upon saving new data
                 website_entry.delete(0, 'end')
                 password_entry.delete(0, 'end')
 
@@ -58,9 +110,9 @@ password_label = Label(text="Password:")
 password_label.grid(column=0, row=3)
 
 # Entries
-website_entry = Entry(width=37)
+website_entry = Entry(width=21)
 website_entry.focus()
-website_entry.grid(column=1, row=1, columnspan=2)
+website_entry.grid(column=1, row=1)
 
 email_username_entry = Entry(width=37)
 email_username_entry.insert(0, "example@example.com")
@@ -70,6 +122,9 @@ password_entry = Entry(width=21)
 password_entry.grid(column=1, row=3)
 
 # Buttons
+search_button = Button(text="Search", width=12, command=find_password)
+search_button.grid(column=2, row=1)
+
 generate_password_button = Button(text="Generate Password", width=12, command=generate_password)
 generate_password_button.grid(column=2, row=3)
 
